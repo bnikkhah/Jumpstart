002f7a262ad0dffd44cbcac16d58893477aa4a8f
Behnam Nikkhah <bnikkhah22@gmail.com>
2017-06-26 13:37:11 -0600

    navbar animation

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,4 +8,29 @@ $(document).ready(function(){
 		slideToShow:1,
 		arrows:false
 	});
+	
+	var lastScrollTop = 0;
+	$(window).scroll(function(){
+		var scrollTop = $(this).scrollTop();
+		if (scrollTop == 0){
+			if($('.toggleNav').hasClass('container')){
+				$('.toggleNav').removeClass('container');
+				$('.toggleNav').addClass('container-fluid');
+			}
+			lastScrollTop = scrollTop;
+		}else if (scrollTop - lastScrollTop > 50){
+			var navbarHeight = $('.navbar').css('height');
+			$('.navbar').animate({
+				top:'-' + navbarHeight
+			}, 150);
+			lastScrollTop = scrollTop;
+		}else if (lastScrollTop - scrollTop > 50){
+			$('.navbar').animate({
+				top:'0px'
+			}, 150);
+			$('.toggleNav').removeClass('container-fluid');
+			$('.toggleNav').addClass('container');
+			lastScrollTop = scrollTop;
+		}
+	});
 });
